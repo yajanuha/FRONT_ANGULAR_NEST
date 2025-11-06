7a9da625631c21ea637e605157c97ef1f5cea51e
yajanuha <yajanuhagrefa@gmail.com>
2025-11-05 23:30:51 -0500

    Primer commit - proyecto Angular actualizado

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.html',
+  styleUrls: ['./login.css'],
+
+})
+export class LoginComponent { }
